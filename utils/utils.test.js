09c6d620c6061fc6ab8c24d93b82662e5c776898
Arun Kumar <arun.kasireddi@gmail.com>
2017-09-12 14:06:45 +0100

    Added using assertions for validations

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,23 +1,43 @@
+const expect = require('expect');
+
 const utils = require('./utils');
+
 
 it('should add two numbers', () => {
     var res = utils.add(33, 11);
-    if (res !== 44) {
-        throw new Error(`Expected 44, but got ${res}`);
-    }
+    expect(res).toBe(44).toBeA('number');
 });
 
 it('should square a number', () => {
     var res = utils.square(4);
-    if (res !== 16) {
-        throw new Error(`Expected 16, but got ${res}`);
-
-    }
+    expect(res).toBe(16).toBeA('number');
 });
 
 it('should subtract two numbers', () => {
     var res = utils.subtract(12, 10);
-    if(res !== 2){
+    if (res !== 2) {
         throw new Error(`Expected 2, but got ${res}`);
     }
 });
+
+it('should verify first and last names are set', () => {
+    var user = { location: 'Dublin', age: 28 };
+    var res = utils.setName(user, "Arun Kumar");
+    expect(res).toInclude({
+        firstName: 'Arun',
+        lastName: 'Kumar'
+    });
+});
+
+// it('should expect some values', () => {
+//     // expect(12).toNotBe(11);
+
+//     // expect([2, 3, 4]).toInclude(4);
+//     expect({
+//         name: 'Arun',
+//         age: 28,
+//         location: 'Dublin'
+//     }).toInclude({
+//         age: 28
+//     })
+// });
